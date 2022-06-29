5695da0716ead25af7192ee5c20d93f268f4dfce
alannarisse <angrypirate@gmail.com>
2022-06-29 15:50:13 -0700

    some minor typos and added console logs

--- HeadphoneScene.js
+++ HeadphoneScene.js
@@ -1,19 +1,17 @@
-class HeadphoneScene extends PhaserScene {
-    constructor() {
-    super({ key:
-      'HeadphoneScene'
-    })
-}
+class HeadphoneScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'HeadphoneScene' })
+  }
+  preload() {
+    this.load.image('hpInfo', 'https://via.placeholder.com/200x150.png')
+  }
 
-preload() {
-  this.load.image('hpInfo', 'https://via.placeholder.com/200x150.png');
-}
+  create() {
+    this.hpInfo = this.add.image(400, 300, 'hpInfo')
+    console.log(`im in headphonescene create`)
+  }
 
-create() {
-  this.hpInfo = this.add.image(400, 300, 'hpInfo');
+  update() {
+    console.log(`im in headphonescene update`)
+  }
 }
-
-update() {
-  
-}
-
