e6b115d2c907007ea4f7e2a97c17a0b8a5b25f40
Emmett C <60626358+emmettcrass@users.noreply.github.com>
2022-07-05 16:53:11 +0000

    fixed typos

--- EduScene.js
+++ EduScene.js
@@ -1,13 +1,13 @@
 class EduScene extends Phaser.Scene {
 	constructor() {
-		super({ key: EduScene })
+		super({ key: 'EduScene' })
 	}
 
 	create() {
 		this.add.text( 150, 250, 'Click to Bath!', {fill: '#000000', fontSize: '20px'})
 		this.input.on('pointerdown', () => {
-			this.scene.stop(EduScene)
-			this.scene.start(BathScene)
+			this.scene.stop('EduScene')
+			this.scene.start('BathScene')
 		})
 	}
 }
