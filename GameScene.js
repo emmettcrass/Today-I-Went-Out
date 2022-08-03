f73034803209fb4609c6381710a97ef3d924d4d4
Emmett C <ecrass@pdx.edu>
2022-08-03 12:50:37 -0700

    commit

--- GameScene.js
+++ GameScene.js
@@ -1,14 +1,16 @@
 class GameScene extends Phaser.Scene {
   constructor() {
-    super({ key: 'GameScene' })
+    super({
+      key: 'GameScene'
+    })
     this.onHeadphones = false
   }
 
   preload() {
-    this.load.image('chair', '/assets/imgs/chair.PNG')
-    this.load.image('table', '/assets/imgs/table.PNG')
-    this.load.image('bed', '/assets/imgs/bed.PNG')
-    this.load.image('tv', '/assets/imgs/tv.PNG')
+    this.load.atlas('player', 'assets/imgs/anim/walk-r/walk-r.json');
+    this.load.image('table', '/assets/imgs/table.PNG');
+    this.load.image('bed', '/assets/imgs/bed.PNG');
+    this.load.image('tv', '/assets/imgs/tv.PNG');
   }
 
   create() {
@@ -17,26 +19,40 @@ class GameScene extends Phaser.Scene {
 
     this.table = this.physics.add.image(700, 162, 'table')
     this.table.setSize(198, 69, true)
-    this.table.body.setOffset(0,0)
+    this.table.body.setOffset(0, 0)
     this.table.setImmovable(true)
+
     this.tv = this.physics.add.image(370, 150, 'tv')
     this.tv.setSize(250, 125, true)
-    this.tv.body.setOffset(0,0)
+    this.tv.body.setOffset(0, 0)
     this.tv.setImmovable(true)
+
     this.bed = this.physics.add.image(84, 295, 'bed')
     this.bed.setSize(165, 325, true)
-    this.bed.body.setOffset(0,0)
+    this.bed.body.setOffset(0, 0)
     this.bed.setImmovable(true)
-    //make chair player
-    this.chair = this.physics.add.image(370, 400, 'chair')
-    //keeps the chair in screen//
-    this.chair.setCollideWorldBounds(true)
+
+    //make player
+    this.player = this.physics.add.sprite(370, 400, 'player', 'assets/imgs/anim/walk-r/r-l-1.png')
+
+    //player animations//
+
+    this.anims.create({
+      key: 'walk-r',
+      frames: this.anims.generateFrameNumbers("walk-r"),
+      frameRate: 6,
+      repeat: -1
+    });
+    player.anims.play('walk-r');
+
+    //keeps the player in screen//
+    this.player.setCollideWorldBounds(true)
 
     //set collisions
-    this.physics.add.collider(this.chair, this.heaphones)
-    this.physics.add.collider(this.chair, this.tv)
-    this.physics.add.collider(this.chair, this.table)
-    this.physics.add.collider(this.chair, this.bed)
+    this.physics.add.collider(this.player, this.heaphones)
+    this.physics.add.collider(this.player, this.tv)
+    this.physics.add.collider(this.player, this.table)
+    this.physics.add.collider(this.player, this.bed)
 
     /*make an align grid
             this.aGrid = new AlignGrid({
@@ -50,7 +66,7 @@ class GameScene extends Phaser.Scene {
           this.aGrid.showNumbers();
           */
 
-    this.physics.add.overlap(this.chair, this.headphones, this.collectItem, null, this)
+    this.physics.add.overlap(this.player, this.headphones, this.collectItem, null, this)
 
     /* function detectOverlap() {
       this.onHeadphones = true
@@ -62,7 +78,10 @@ class GameScene extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys()
 
     //lets change scenes
-    this.add.text(300, 20, 'Click to edu!', { fill: '#ffffff', fontSize: '20px' })
+    this.add.text(300, 20, 'Click to edu!', {
+      fill: '#ffffff',
+      fontSize: '20px'
+    })
     this.input.on('pointerdown', () => {
       this.scene.stop('GameScene')
       this.scene.start('EduScene')
@@ -71,23 +90,32 @@ class GameScene extends Phaser.Scene {
 
   update() {
     // Update based on keypress here!
-    this.chair.setVelocity(0)
+    this.player.setVelocity(0)
 
     if (this.cursors.right.isDown) {
-      this.chair.setVelocityX(150)
+      this.player.setVelocityX(150)
+    } else if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-150)
+    } else {
+      this.player.setVelocityX(0);
     }
-    if (this.cursors.left.isDown) {
-      this.chair.setVelocityX(-150)
-    }
+
     if (this.cursors.up.isDown) {
-      this.chair.setVelocityY(-150)
+      this.player.setVelocityY(-150)
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(150)
+    } else {
+      this.player.setVelocityY(0)
     }
-    if (this.cursors.down.isDown) {
-      this.chair.setVelocityY(150)
+
+    if (this.player.body.velocity.x !== 0 || this.player.body.velocity.y !== 0) {
+      this.player.play('walk-r', true);
+    } else {
+      this.player.play('idle', true);
     }
   }
   //this destroys the headphone item when collected, pauses GameScene and starts HeadphoneScene
-  collectItem(chair, headphones) {
+  collectItem(player, headphones) {
     headphones.destroy()
     this.onHeadphones = true
     console.log(`made it to collectitem`)
