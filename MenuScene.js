e1406551bf2ff0b432e3b4e39303cc11e80fda36
Emmett C <ecrass@pdx.edu>
2022-09-23 13:48:25 -0700

    menu scene has button that goes to next scene

--- MenuScene.js
+++ MenuScene.js
@@ -2,10 +2,18 @@ class MenuScene extends Phaser.Scene {
 	constructor() {
 		super({ key: 'MenuScene' })
 	}
-
+	preload() {
+		this.load.image('messageBox', '/assets/imgs/messageBox.png');
+		this.load.image('nextButton', '/assets/imgs/nextButton.png');
+	  }
 	create() {
-		this.add.text( 150, 250, 'Click to game!', {fill: '#000000', fontSize: '20px'})
-		this.input.on('pointerdown', () => {
+		this.messageBox = this.add.image(400, 270, 'messageBox')
+		this.nextButton = this.add.image(490, 365, 'nextButton').setInteractive();
+
+
+		
+		this.add.text( 350, 250, '!', {fill: '#000000', fontSize: '20px'})
+		this.nextButton.on('pointerdown', () => {
 			this.scene.stop('MenuScene')
 			this.scene.start('GameScene')
 		})
